14984ce6069abdafd3574a6c2554f82450d0d654
Alex Cook <a5cook@uwaterloo.ca>
2025-11-04 15:06:34 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...}:
+    let
+      lib = nixpkgs.lib;
+    in
+    {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix
+                    home-manager.nixosModules.home-manager];
+      };
+    };
+  };
+}
