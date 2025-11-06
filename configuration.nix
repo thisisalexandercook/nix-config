ba66ed9c09d5fdc5e795833111ed50c2828cf1f7
Alex Cook <a5cook@uwaterloo.ca>
2025-11-06 12:57:41 -0500

    refactor: change emacs to a service

--- configuration.nix
+++ configuration.nix
@@ -129,10 +129,12 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Emacs service
+  services.emacs.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    emacs
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
