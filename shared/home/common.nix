8a222bc9d329be480d3039194c87117f278f20d9
Alex Cook <a5cook@uwaterloo.ca>
2025-11-13 11:14:04 -0500

    chore(formatting): home ssh formatting

--- shared/home/common.nix
+++ shared/home/common.nix
@@ -103,14 +103,13 @@ services.syncthing = {
 };
 
 programs.ssh = {
-    enable = true;
+  enable = true;
 
-    matchBlocks = {
-      "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "${config.sops.secrets.github_key.path}";
-      };
+  matchBlocks = {
+    "github.com" = {
+      hostname = "github.com";
+      user = "git";
+      identityFile = "${config.sops.secrets.github_key.path}";
     };
   };
 
