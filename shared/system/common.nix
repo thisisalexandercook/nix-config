a13da2176ec42284e25ab2cc5d4137c971066212
Alex Cook <a5cook@uwaterloo.ca>
2025-11-25 11:29:10 -0500

    feat(system.common): add automated garbage collection

--- shared/system/common.nix
+++ shared/system/common.nix
@@ -129,6 +129,12 @@
     allowedUDPPorts = [ 22000 21027 ];
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 10d";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
