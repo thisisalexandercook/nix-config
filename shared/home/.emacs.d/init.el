d16708291b0db574ed78a0d1b321f00c1e7c2f31
Alex Cook <a5cook@uwaterloo.ca>
2025-12-03 12:38:25 -0500

    config(emacs): set file for custom vars

--- shared/home/.emacs.d/init.el
+++ shared/home/.emacs.d/init.el
@@ -327,19 +327,7 @@
    :immediate-finish t))
   )
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(beframe company-coq consult denote-journal direnv eat envrc
-	     eshell-prompt-extras fontaine groovy-mode magit
-	     marginalia markdown-mode nix-mode nov orderless
-	     proof-general vertico yaml-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; custom file
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
